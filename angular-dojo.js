feac969e71ad0a811458d42e8e0f5cc0be70160e
HaoJu Zheng <zhenghaoju700@163.com>
2014-12-22 18:19:32 +0800

    add dojo-process-bar and after-init event

--- angular-dojo.js
+++ angular-dojo.js
@@ -1,77 +1,109 @@
 /**
-* angular-dojo Module
-*/ 
+ * angular-dojo Module
+ */
 angular.module('angular-dojo', [])
-.directive("dojoParser", function(){
-	return {
-		restrict: 'A',
-		scope: false,
-		controller: function($scope, $element, $attrs, $transclude){
-			var _this = this;
-			_this.dojoInstances = _this.dojoInstances || [];
-			$element.wrap("<span></span>");
-			var parentNode = $element[0].parentNode; 
-			require(["dojo/parser","dojo/domReady!"], function(parser){
-                 parser.parse(parentNode).then(function(instances){
-                 	$(parentNode).children().unwrap();
-                 	angular.forEach(instances, function(instance){
-						_this.dojoInstances.push(instance);
-                 	});
-                 });
-			});
-			$scope.dojoInstances = _this.dojoInstances;
-		}
-	};
-}).directive('dojoButton', function(){
-	return {
-		restrict: 'E',
-		template: '<div></div>',
-		replace: true,
-		scope: {
-			dojoConfig: "="
-		}, 
-		controller: function($scope, $element, $attrs, $transclude){
-			require(["dijit/form/Button"], function(Button){
-				new Button($scope.dojoConfig, $element[0]).startup();
-			}); 
-		}
-	};
-}).directive('dojoNumberSpinner', function(){
-	return {
-		restrict: 'E',
-		template: '<div></div>',
-		replace: true,
-		scope: {
-			dojoConfig: "="
-		}, 
-		controller: function($scope, $element, $attrs, $transclude){
-			require(["dijit/form/NumberSpinner"], function(NumberSpinner){
-				new NumberSpinner($scope.dojoConfig, $element[0]).startup();
-			}); 
-		}
-	};
-}).directive('dojoMenuButton', function(){
-	// Runs during compile
-	return {
-		restrict: 'E',
-		template: '<div></div>',
-		replace: true,
-		scope: {
-			dojoConfig: "="
-		}, 
-		controller: function($scope, $element, $attrs, $transclude){
-			require(["dijit/form/DropDownButton", "dijit/DropDownMenu", "dijit/MenuItem"], 
-				function(DropDownButton, DropDownMenu, MenuItem){
-				var menu = new DropDownMenu($scope.dojoConfig.dropDownMenu);
-				angular.forEach($scope.dojoConfig.menuItems,function(menuItem){
- 					var menuItem = new MenuItem(menuItem);
-			   		menu.addChild(menuItem);
-				});
-				menu.startup();
-				$scope.dojoConfig.dropDownButton.dropDown = menu;
-			    var button = new DropDownButton($scope.dojoConfig.dropDownButton, $element[0]);
-			    button.startup();
-			}); 
-		}
-	};
-});
+    .directive("dojoParser", function(){
+        return {
+            restrict: 'A',
+            scope: false,
+            controller: function($scope, $element, $attrs, $transclude){
+                var _this = this;
+                _this.dojoInstances = _this.dojoInstances || [];
+                $element.wrap("<span></span>");
+                var parentNode = $element[0].parentNode;
+                require(["dojo/parser","dojo/domReady!"], function(parser){
+                    parser.parse(parentNode).then(function(instances){
+                        $(parentNode).children().unwrap();
+                        angular.forEach(instances, function(instance){
+                            _this.dojoInstances.push(instance);
+                        });
+                    });
+                });
+                $scope.dojoInstances = _this.dojoInstances;
+            }
+        };
+    }).directive('dojoButton', function(){
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            replace: true,
+            scope: {
+                dojoConfig: "="
+            },
+            controller: function($scope, $element, $attrs, $transclude){
+                require(["dijit/form/button"], function(Button){
+                    new Button($scope.dojoConfig, $element[0]).startup();
+                });
+            }
+        };
+    }).directive('dojoNumberSpinner', function(){
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            replace: true,
+            scope: {
+                dojoConfig: "=",
+                afterInit: "&"
+            },
+            controller: function($scope, $element, $attrs, $transclude){
+                require(["dijit/form/NumberSpinner"], function(NumberSpinner){
+                    var numberSpinner = new NumberSpinner($scope.dojoConfig, $element[0]);
+                    numberSpinner.startup();
+
+                    if($scope.afterInit()){
+                        angular.bind(numberSpinner, $scope.afterInit())();
+                    }
+                });
+            }
+        };
+    }).directive('dojoMenuButton', function(){
+        // Runs during compile
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            replace: true,
+            scope: {
+                dojoConfig: "=",
+                afterInit: "&"
+            },
+            controller: function($scope, $element, $attrs, $transclude){
+                require(["dijit/form/DropDownButton", "dijit/DropDownMenu", "dijit/MenuItem"],
+                    function(DropDownButton, DropDownMenu, MenuItem){
+                        var menu = new DropDownMenu($scope.dojoConfig.dropDownMenu);
+                        angular.forEach($scope.dojoConfig.menuItems,function(menuItem){
+                            var menuItem = new MenuItem(menuItem);
+                            menu.addChild(menuItem);
+                        });
+                        menu.startup();
+                        $scope.dojoConfig.dropDownButton.dropDown = menu;
+                        var button = new DropDownButton($scope.dojoConfig.dropDownButton, $element[0]);
+                        button.startup();
+
+                        if($scope.afterInit()){
+                            angular.bind(button, $scope.afterInit())();
+                        }
+                    });
+            }
+        };
+    }).directive('dojoProgressBar', function(){
+        // Runs during compile
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            replace: true,
+            scope: {
+                dojoConfig: "=",
+                afterInit: "&"
+            },
+            controller: function($scope, $element, $attrs, $transclude){
+                require(["dijit/ProgressBar"], function(ProgressBar){
+                    var progressBar = new ProgressBar($scope.dojoConfig).placeAt($element[0]);
+                    progressBar.startup();
+                    //run callback method;
+                    if($scope.afterInit()){
+                        angular.bind(progressBar, $scope.afterInit())();
+                    }
+                });
+            }
+        };
+    });
